318e9b09e60de326ff1d1651079d79d9e7590ad4
wilembergson <wilembergson@yahoo.com>
2022-07-28 01:07:44 -0300

    Upvote and Downvote tests

--- back-end/tests/factories/recommendationFactory.ts
+++ back-end/tests/factories/recommendationFactory.ts
@@ -9,6 +9,16 @@ export async function createRecommendation(){
   return recommendation
 }
 
+async function createDataRecommendation(){
+  const data = await prisma.recommendation.create({
+    data: {
+      name:faker.name.findName(),
+    youtubeLink:"https://www.youtube.com/watch?v=-SIjTlOJUkI&ab_channel=TarcisiodoAcordeon"
+    }
+  });
+  return data
+}
+
 export async function createRecommendationWithoutLink(){
   const recommendation = {
     name:faker.name.findName()
@@ -33,4 +43,30 @@ export async function deleteAll(){
     await prisma.$transaction([
         prisma.$executeRaw`TRUNCATE TABLE recommendations`
     ])
+}
+
+export async function getExistingRecommendation(){
+  const recommendation = await createDataRecommendation()
+  return recommendation
+}
+
+export async function lessFiveRecomendation(){
+  const recommendation = await createDataRecommendation()
+  await prisma.recommendation.update({
+    where: { 
+      id:recommendation.id
+     },
+    data: {
+      score: -5,
+    },
+  });
+  return recommendation.id
+}
+export async function findById(id:number){
+  const result = await prisma.recommendation.findFirst({
+    where:{
+      id
+    }
+  })
+  return result
 }
--- back-end/tests/recommendation.test.ts
+++ back-end/tests/recommendation.test.ts
@@ -1,9 +1,10 @@
+import { faker } from "@faker-js/faker"
 import supertest from "supertest"
 
 import app from "../src/app.js"
-import { createRecommendation, createRecommendationWithoutLink, createRecommendationWithoutName, createRecommendationWithoutYoutubeLink, deleteAll } from "./factories/recommendationFactory.js"
+import { createRecommendation, createRecommendationWithoutLink, createRecommendationWithoutName, createRecommendationWithoutYoutubeLink, deleteAll, findById, getExistingRecommendation, lessFiveRecomendation } from "./factories/recommendationFactory.js"
 const agent = supertest(app)
-describe("POST /", () => {
+describe("POST /recommendations", () => {
     beforeEach(async ()=>{
         await deleteAll()
     })
@@ -37,5 +38,46 @@ describe("POST /", () => {
         const recommendation = await createRecommendationWithoutYoutubeLink()
         const response = await agent.post('/recommendations').send(recommendation);
         expect(response.status).toBe(422);
+    });
+})
+
+describe("POST /recommendations/:id/upvote", () => {
+    beforeEach(async ()=>{
+        await deleteAll()
+    })
+    it("Adiciona ponto a recomendação - deve retornar 200", async () => {
+        const recommendation = await getExistingRecommendation()
+        const id = recommendation.id
+        const response = await agent.post(`/recommendations/${id}/upvote`);
+        expect(response.status).toBe(200);
+    });
+    it("Não encontra recomendação com o ID informado - deve retornar 404", async () => {
+        const id = faker.datatype.number()
+        const response = await agent.post(`/recommendations/${id}/upvote`);
+        expect(response.status).toBe(404);
+    });
+})
+
+describe("POST /recommendations/:id/downvote", () => {
+    beforeEach(async ()=>{
+        await deleteAll()
+    })
+    it("Subtrai ponto da recomendação - deve retornar 200", async () => {
+        const recommendation = await getExistingRecommendation()
+        const id = recommendation.id
+        const response = await agent.post(`/recommendations/${id}/downvote`);
+        expect(response.status).toBe(200);
+    });
+    it("Não encontra recomendação com o ID informado - deve retornar 404", async () => {
+        const id = faker.datatype.number()
+        const response = await agent.post(`/recommendations/${id}/upvote`);
+        expect(response.status).toBe(404);
+    });
+    it("Deleta a recomendação que tiver pontuação abaixo de (-5) - deve retornar 200", async () => {
+        const id = await lessFiveRecomendation()
+        const response = await agent.post(`/recommendations/${id}/downvote`);
+        const recommendation = await findById(id)
+        expect(recommendation).toBeNull()
+        expect(response.status).toBe(200);
     });
 })
