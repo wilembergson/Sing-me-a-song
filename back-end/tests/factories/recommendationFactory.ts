731f2a3b7835a7c6324077b48716b5fd1141a8e3
wilembergson <wilembergson@yahoo.com>
2022-07-27 23:40:50 -0300

    Post recommendations route tests

--- back-end/tests/factories/recommendationFactory.ts
+++ back-end/tests/factories/recommendationFactory.ts
@@ -9,6 +9,26 @@ export async function createRecommendation(){
   return recommendation
 }
 
+export async function createRecommendationWithoutLink(){
+  const recommendation = {
+    name:faker.name.findName()
+  }
+  return recommendation
+}
+
+export async function createRecommendationWithoutYoutubeLink(){
+  const recommendation = {
+    name:faker.name.findName(),
+    youtubeLink: faker.internet.url()
+  }
+  return recommendation
+}
+export async function createRecommendationWithoutName(){
+  const recommendation = {
+    youtubeLink:"https://www.youtube.com/watch?v=-SIjTlOJUkI&ab_channel=TarcisiodoAcordeon"
+  }
+  return recommendation
+}
 export async function deleteAll(){
     await prisma.$transaction([
         prisma.$executeRaw`TRUNCATE TABLE recommendations`
--- back-end/tests/recommendation.test.ts
+++ back-end/tests/recommendation.test.ts
@@ -1,10 +1,10 @@
 import supertest from "supertest"
 
 import app from "../src/app.js"
-import { createRecommendation, deleteAll } from "./factories/recommendationFactory.js"
+import { createRecommendation, createRecommendationWithoutLink, createRecommendationWithoutName, createRecommendationWithoutYoutubeLink, deleteAll } from "./factories/recommendationFactory.js"
 const agent = supertest(app)
 describe("POST /", () => {
-    beforeAll(async ()=>{
+    beforeEach(async ()=>{
         await deleteAll()
     })
     it("Nova recomendação criada - deve retornar 201", async () => {
@@ -17,5 +17,25 @@ describe("POST /", () => {
         await supertest.agent(app).post('/recommendations').send(recommendation);
         const response = await agent.post('/recommendations').send(recommendation);
         expect(response.status).toBe(409);
+    });
+    it("Recomendação não enviada - deve retornar 422", async () => {
+        const recommendation = undefined
+        const response = await agent.post('/recommendations').send(recommendation);
+        expect(response.status).toBe(422);
+    });
+    it("Recomendação enviada sem o nome - deve retornar 422", async () => {
+        const recommendation = await createRecommendationWithoutName()
+        const response = await agent.post('/recommendations').send(recommendation);
+        expect(response.status).toBe(422);
+    });
+    it("Recomendação enviada sem o link - deve retornar 422", async () => {
+        const recommendation = await createRecommendationWithoutLink()
+        const response = await agent.post('/recommendations').send(recommendation);
+        expect(response.status).toBe(422);
+    });
+    it("Recomendação enviada sem o link do youtube válido - deve retornar 422", async () => {
+        const recommendation = await createRecommendationWithoutYoutubeLink()
+        const response = await agent.post('/recommendations').send(recommendation);
+        expect(response.status).toBe(422);
     });
 })
