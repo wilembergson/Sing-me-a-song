55f84784c9ba895209519668a8073996ebbfc682
wilembergson <wilembergson@yahoo.com>
2022-08-01 02:26:31 -0300

    Tests cypress complete

--- front-end/cypress/e2e/addNewSong.cy.js
+++ front-end/cypress/e2e/addNewSong.cy.js
@@ -2,16 +2,17 @@
 const url = "http://localhost:3000";
 
 describe("Recomendações", () => {
-    it("Deve adicionar uma recomendação", () => {
+    it("Deve adicionar uma recomendação; deve aumentar um ponto e diminuir na sequencia", () => {
 
         const recommendation = {
-            name: "Duro igual concreto",
+            name: "Duro igual concreto3",
             youtubeUrl: "https://www.youtube.com/watch?v=vTKP99Kll5E&ab_channel=1KiloOficial"
         }
-        
         cy.visit(url);
         cy.get("#new-name").type(recommendation.name);
         cy.get("#new-youtubeUrl").type(recommendation.youtubeUrl);
         cy.get("#new-createButton").click();
+        cy.get("#upvote").click();
+        cy.get("#downvote").click();
     });
 });
